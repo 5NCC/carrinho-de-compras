92ed96b557514eb7c4f07359652fccd8ff2df63b
Kaê Coutinho (Macbook Pro BEPiD) <coutinho.kae2@gmail.com>
2014-10-06 13:03:53 -0300

    Testando JUnit

---
+++ test/testes/Teste_1.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Teste_1 extends Object
+{
+    @Test
+    public void exemplo()
+    {
+        Integer a = 1, b = 2;
+        assertEquals(3,a + b);
+    }
+}
